e7e3af0ff3c0271eba80dea317606d761aae6c17
Hoth Mathew <hmathew@su-h4.21-school.ru>
2020-01-11 17:12:30 +0300

    start

---
+++ Makefile
@@ -0,0 +1,37 @@
+PROJ_NAME=ASM
+NAME = asm
+SRC = src
+CC = gcc
+FLAGS = -g
+LIBFTDIR = libft
+LIBFT = $(LIBFTDIR)/libft.a
+SRCS = $(shell find $(SRC) -type f | grep -E "\.c$$")
+INC = $(LIBFTDIR)/inc inc
+OBJS = $(SRCS:.c=.o)
+
+.PHONY: all clean fclean re
+
+all: $(NAME)
+
+%.o: %.c
+	@echo "\033[0;32m [$(PROJ_NAME)] \033[0m   \033[0;33m Compiling:\033[0m" $<
+	@$(CC) $(FLAGS) $(addprefix -I,$(INC)) -o $@ -c $<
+
+$(NAME): $(LIBFT) $(OBJS)
+	@echo "\033[0;32m [$(PROJ_NAME)] \033[0m   \033[0;33m Compiling binary:\033[0m" $@
+	@$(CC) $(FLAGS) -fprofile-arcs -ftest-coverage $(addprefix -I,$(INC)) $(LIBFT) $(OBJS) -o $(NAME)
+
+$(LIBFT):
+	@make -C $(LIBFTDIR)
+
+clean:
+	@echo "\033[0;32m [$(PROJ_NAME)] \033[0m   \033[0;33m Clean objects $(PROJ_NAME) \033[0m" $<
+	@rm -rf $(OBJS)
+	@make -C $(LIBFTDIR) clean
+
+fclean: clean
+	@echo "\033[0;32m [$(PROJ_NAME)] \033[0m   \033[0;33m Clean bin $(PROJ_NAME) \033[0m" $<
+	@rm -rf $(NAME)
+	@make -C $(LIBFTDIR) fclean
+
+re: fclean all
---
+++ src/main.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hmathew <hmathew@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/12/22 18:03:42 by hmathew           #+#    #+#             */
+/*   Updated: 2020/01/11 17:07:51 by hmathew          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+#include "ft_printf.h"
+
+static void	print_usage(void)
+{
+	ft_printf("Usage: ./asm [-a] [*] <champion.s>\n");
+	exit(0);
+}
+
+int		main(int argc, char const *argv[])
+{
+	return 0;
+}
