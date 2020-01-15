ce5f5b1ba58da5fd19fb2a1c5282991c34e5cce8
Midas Trisha <mtrisha@student.21-school.ru>
2020-01-15 13:33:28 +0300

    add op.h op.c from my corewar_vm

--- inc/op.h
+++ inc/op.h
@@ -3,59 +3,78 @@
 /*                                                        :::      ::::::::   */
 /*   op.h                                               :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: hmathew <hmathew@student.42.fr>            +#+  +:+       +#+        */
+/*   By: mtrisha <mtrisha@student.21-school.ru>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2013/10/04 11:33:27 by zaz               #+#    #+#             */
-/*   Updated: 2019/12/22 18:52:30 by hmathew          ###   ########.fr       */
+/*   Updated: 2020/01/14 16:45:21 by mtrisha          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#define IND_SIZE				2
-#define REG_SIZE				4
-#define DIR_SIZE				REG_SIZE
+#ifndef OP_H
+# define OP_H
 
-#define REG_CODE				1
-#define DIR_CODE				2
-#define IND_CODE				3
+# define IND_SIZE				2
+# define REG_SIZE				4
+# define DIR_SIZE				REG_SIZE
 
-#define MAX_ARGS_NUMBER			4
-#define MAX_PLAYERS				4
-#define MEM_SIZE				(4*1024)
-#define IDX_MOD					(MEM_SIZE / 8)
-#define CHAMP_MAX_SIZE			(MEM_SIZE / 6)
+# define REG_CODE				1
+# define DIR_CODE				2
+# define IND_CODE				3
 
-#define COMMENT_CHAR			'#'
-#define LABEL_CHAR				':'
-#define DIRECT_CHAR				'%'
-#define SEPARATOR_CHAR			','
+# define MAX_ARGS_NUMBER			4
+# define MAX_PLAYERS				4
+# define MEM_SIZE				(4*1024)
+# define IDX_MOD					(MEM_SIZE / 8)
+# define CHAMP_MAX_SIZE			(MEM_SIZE / 6)
 
-#define LABEL_CHARS				"abcdefghijklmnopqrstuvwxyz_0123456789"
+# define COMMENT_CHAR			'#'
+# define LABEL_CHAR				':'
+# define DIRECT_CHAR				'%'
+# define SEPARATOR_CHAR			','
 
-#define NAME_CMD_STRING			".name"
-#define COMMENT_CMD_STRING		".comment"
+# define LABEL_CHARS				"abcdefghijklmnopqrstuvwxyz_0123456789"
 
-#define REG_NUMBER				16
+# define NAME_CMD_STRING			".name"
+# define COMMENT_CMD_STRING		".comment"
 
-#define CYCLE_TO_DIE			1536
-#define CYCLE_DELTA				50
-#define NBR_LIVE				21
-#define MAX_CHECKS				10
+# define REG_NUMBER				16
 
-typedef chart_arg_type;
+# define CYCLE_TO_DIE			1536
+# define CYCLE_DELTA				50
+# define NBR_LIVE				21
+# define MAX_CHECKS				10
 
-#define T_REG					1
-#define T_DIR					2
-#define T_IND					4
-#define T_LAB					8
+typedef char	t_arg_type;
+
+# define T_REG					1
+# define T_DIR					2
+# define T_IND					4
+# define T_LAB					8
 
 # define PROG_NAME_LENGTH		(128)
 # define COMMENT_LENGTH			(2048)
 # define COREWAR_EXEC_MAGIC		0xea83f3
 
-typedef struct header_s
+typedef struct		s_header
 {
 	unsigned int	magic;
 	char			prog_name[PROG_NAME_LENGTH + 1];
 	unsigned int	prog_size;
 	char			comment[COMMENT_LENGTH + 1];
-}	header_t;
+}					t_header;
+
+typedef struct		s_op
+{
+	char			*cmd;
+	int				count_of_args;
+	int				arg_type[3];
+	int				op_code;
+	int				time_in_cycles;
+	char			*describe;
+	int				args_code;
+	int				short_tdir;
+}					t_op;
+
+extern t_op			g_op_tab[17];
+
+#endif
